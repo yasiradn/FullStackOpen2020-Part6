b01ae2a9c5f829f761054f32f36d2b9df3a1ed15
yasir <aff3ad@gmail.com>
2020-04-12 18:41:15 +0300

    6.1+6.2 unicafe revisited

--- src/reducer.js
+++ src/reducer.js
@@ -1,20 +1,21 @@
 const initialState = {
-  good: 0,
-  ok: 0,
-  bad: 0
+    good: 0,
+    ok: 0,
+    bad: 0
 }
 
 const counterReducer = (state = initialState, action) => {
   console.log(action)
+  console.log(state)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return state = {...state, good: state.good + 1}
     case 'OK':
-      return state
+      return state = {...state, ok:state.ok+1}
     case 'BAD':
-      return state
+      return state = {...state,bad:state.bad+1}
     case 'ZERO':
-      return state
+      return state = {...state,initialState}
     default: return state
   }
   
