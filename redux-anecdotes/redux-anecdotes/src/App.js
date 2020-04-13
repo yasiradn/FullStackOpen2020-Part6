712debabbab21989fb275f71929e42cb8e967cfd
yasir <aff3ad@gmail.com>
2020-04-13 10:46:48 +0300

    6.3 anecdotes setp1

---
+++ redux-anecdotes/redux-anecdotes/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    const getIndex = anecdotes.findIndex(anecdotes => anecdotes.id === id)
+    const getVote = anecdotes[getIndex].votes +=1
+    dispatch({type:'vote',content:anecdotes[getIndex].content, id:id, votes: getVote})
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
